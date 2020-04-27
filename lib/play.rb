eb1f36515abfd6375dd5d8650f1418720ce148e0
Sarah Peters <sarahpieters15@gmail.com>
2020-04-27 14:55:48 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,8 +37,8 @@ end
 
 # Define your play method below
 def play(board)
-  board = 0
-  until board == 9
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  until board == ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
     puts "Please enter 1-9:"
     board += 1
   end

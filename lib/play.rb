b3a1cf5c6c7f25e946c035ddd1770112d3d6901f
Sarah Peters <sarahpieters15@gmail.com>
2020-04-27 14:57:27 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  board = 0
-  until board == 9
-    puts "Please enter 1-9:"
-    board += 1
+  counter = 0
+  until counter == 9
+    turn(board)
+    counter += 1
   end
 end
